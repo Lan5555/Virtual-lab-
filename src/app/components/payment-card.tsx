ce7f73a5e7df6e188b3d6d49383fc78d7fe8f6c2
lan5555 <okekejohnson24@gmail.com>
2024-12-03 12:27:11 +0100

    update

--- src/app/components/payment-card.tsx
+++ src/app/components/payment-card.tsx
@@ -1,5 +1,6 @@
 import { faInfo, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
 interface props{
     header?:string;
@@ -8,28 +9,61 @@ interface props{
     features?:string[];
     currencySymbol?: string;
     onPressed?:() => void;
+    
 }
 const PaymentCard:React.FC<props> = ({header,price,limit,features, currencySymbol, onPressed}) => {
+    const [mediaquery, setMediaQuery] = useState('desktop');
+    const updateMediaQuery = () => {
+        if (window.matchMedia('(max-width: 600px)').matches) {
+          setMediaQuery('mobile');
+        } else if (window.matchMedia('(min-width: 548px) and (max-width:1140px)').matches) {
+          setMediaQuery('tablet');
+        } else {
+          setMediaQuery('desktop');
+        }
+      };
+    
+      useEffect(() => {
+        updateMediaQuery();
+        window.addEventListener('resize', updateMediaQuery);
+        return () => window.removeEventListener('resize', updateMediaQuery);
+      }, []);
+      
     const symbol = currencySymbol ? currencySymbol : "₦";
     return (
-        <div className="p-4 my-2 mr-5 flex justify-start flex-col shadow-2xl gap-0 w-52 rounded-xl bg-white hover:shadow-slate-400 flex-shrink-0">
-            <div className="flex gap-2 relative top-8">
+        <div className={mediaquery == "desktop" ? "p-4 my-2 mr-5 flex justify-start flex-col shadow-2xl gap-0 w-52 rounded-xl bg-white hover:shadow-slate-400 flex-shrink-0"
+            : mediaquery == "tablet" ? "p-3 my-2 mr-5 flex justify-start flex-col shadow-2xl gap-0 w-44 rounded-xl bg-white hover:shadow-slate-400 flex-shrink-0 relative -left-7" : 
+            mediaquery == "mobile" ? "p-3 my-2 mr-5 flex flex-col shadow-2xl gap-0 w-28 h-36 rounded-xl bg-white hover:shadow-slate-400 flex-shrink-0 relative -left-7" : ''
+        }>
+            <div className={mediaquery == "desktop" || mediaquery == "tablet" ? "flex gap-2 relative top-8"
+                : ""
+            }>
                     <FontAwesomeIcon icon={faInfoCircle} className="text-blue-500"></FontAwesomeIcon>
                     <h6 className="relative -top-5 text-blue-700 animate-pulse">{header}</h6>
                 </div>
             <div className="flex gap-3">
-                <h2 className="relative ml-2">{`${symbol}${price}`}</h2>
-                <p className="text-xxs relative top-5 opacity-50">{`/${limit}`}</p>
+                <h2 className={
+                    mediaquery == "desktop" || mediaquery == "tablet" ?
+                    "relative ml-2": 'absolute top-9'
+                }>{`${symbol}${price}`}</h2>
+                <p className={
+                     mediaquery == "desktop" || mediaquery == "tablet" ?
+                    "text-xxs relative top-5 opacity-50":'text-xxs absolute top-20 opacity-50'
+                    }>{`/${limit}`}</p>
             </div>
-            <div>
+            { mediaquery == "desktop" || mediaquery == "tablet" ?(<div>
                 <ul className="relative -left-4 marker:text-yellow-400">
                     {features?.map((element,index) => (
                         <li key={index} className="text-xs font-bold my-2">{element}</li>
                     ))}
                 </ul>
-            </div>
+            </div>):<FontAwesomeIcon icon={faInfoCircle} className="text-green animate-pulse absolute" title="Details"></FontAwesomeIcon>}
             <br></br>
-            <button  className="p-3 w-full text-center bg-green-700 text-white rounded-lg border-none hover:bg-gray-500 shadow-sm cursor-pointer" onClick={onPressed}>
+            <button  className={
+                 mediaquery == "desktop" || mediaquery == "tablet" ?
+                "p-3 w-full text-center bg-green-700 text-white rounded-lg border-none hover:bg-gray-500 shadow-sm cursor-pointer"
+                : 'p-1 mt-1 border-none shadow rounded bg-green-600 text-white'
+            } onClick={onPressed}>
                 Get Plan
             </button>
         </div>
--- src/app/pages/subscription/page.tsx
+++ src/app/pages/subscription/page.tsx
@@ -3,8 +3,28 @@
 import PaymentCard from "@/app/components/payment-card";
 import Wrap from "@/app/components/wrapper";
 import PageLayout from "@/app/page-layouts/page-layout";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
 const Subscription: React.FC = () => {
+    const [mediaquery, setMediaQuery] = useState('desktop');
+    const updateMediaQuery = () => {
+        if (window.matchMedia('(max-width: 600px)').matches) {
+          setMediaQuery('mobile');
+        } else if (window.matchMedia('(min-width: 548px) and (max-width:1140px)').matches) {
+          setMediaQuery('tablet');
+        } else {
+          setMediaQuery('desktop');
+        }
+      };
+    
+      useEffect(() => {
+        updateMediaQuery();
+        window.addEventListener('resize', updateMediaQuery);
+        return () => window.removeEventListener('resize', updateMediaQuery);
+      }, []);
+      
     const plans = [{
         header: "Basic Plan",
         price: 5000,
@@ -30,11 +50,17 @@ const Subscription: React.FC = () => {
                 </Wrap>
             </div>
 
-            <div className="flex justify-center items-center gap-16  shadow-2xl p-28 rounded-3xl" style={{
+            { mediaquery == "desktop" || mediaquery == "tablet" ? (<div className="flex justify-center items-center gap-16  shadow-2xl p-28 rounded-3xl" style={{
                 background:'linear-gradient(to right, white,green)'
             }}>
-                <h3 className="absolute top-32 left-80 ml-5  ">Choose your plan</h3>
-                <small className="absolute top-44 left-80 ml-5 opacity-50 text-xxs">Term&apos;s and conditions applies</small>
+                <h3 className={
+                    mediaquery == "desktop" ? "absolute top-32 left-80 ml-5"
+                    : mediaquery == "tablet" ? "hidden" : 'absolute top-32 left-14'
+                }>Choose your plan</h3>
+                <small className={
+                    mediaquery == "desktop" ? "absolute top-44 left-80 ml-5 opacity-50 text-xxs"
+                    :mediaquery == "tablet" ? "hidden" : 'absolute top-44 left-14 opacity-50 text-xxs'
+                }>Term&apos;s and conditions applies</small>
                 <div className="absolute right-20 top-32 flex gap-4">
                 <button className="w-auto h-auto p-2 rounded-md bg-white border-none border-green-500 shadow-2xl cursor-pointer hover:bg-slate-600">Add ons</button>
                 <button className="w-auto h-auto p-2 rounded-md bg-green-400 border-none shadow cursor-pointer hover:bg-slate-600">Query transaction</button>
@@ -47,11 +73,42 @@ const Subscription: React.FC = () => {
                             price={it.price}
                             features={it.features}
                             limit={it.limit}>
+                            
                         </PaymentCard>
                     </div>
                 ))}
                 
-            </div>
+            </div>):
+            <div className="flex justify-center items-center mt-16">
+            <div className="flex justify-center items-center gap-5 flex-col">
+                <div className="w-96 h-32 shadow-2xl rounded flex gap-4 justify-evenly items-center p-1 ml-1 r-b-b l-b-b hover:shadow">
+                <FontAwesomeIcon icon={faInfoCircle} className="animate-pulse text-green-400"></FontAwesomeIcon>
+                <h3>Basic Plan</h3>
+                <div className="">
+                    <h3>5000/<small>month</small></h3>
+                </div>
+                <button className="p-2 rounded bg-green-500 border-none">Purchace</button>
+                </div>
+                
+                <div className="w-96 h-32 shadow-2xl rounded flex gap-4 justify-evenly items-center p-1 ml-1 l-b-r r-b-r hover:shadow">
+                <FontAwesomeIcon icon={faInfoCircle} className="animate-pulse text-green-400"></FontAwesomeIcon>
+                <h3>Basic Plan</h3>
+                <div className="">
+                    <h3>10000/<small>month</small></h3>
+                </div>
+                <button className="p-2 rounded bg-green-500 border-none">Purchace</button>
+                </div>
+                <div className="w-96 h-32 shadow-2xl rounded flex gap-4 justify-evenly items-center p-1 ml-1 l-b-g r-b-g hover:shadow">
+                <FontAwesomeIcon icon={faInfoCircle} className="animate-pulse text-green-400"></FontAwesomeIcon>
+                <h3>Basic Plan</h3>
+                <div className="">
+                    <h3>50000/<small>month</small></h3>
+                </div>
+                <button className="p-2 rounded bg-green-500 border-none">Purchace</button>
+                </div>
+                </div>
+                </div>
+                }
         </PageLayout>
     )
 };
